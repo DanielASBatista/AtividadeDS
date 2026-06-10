66010a3782beaad4e4a215ade8650a8d702ee780
Rafael Martins Nogueira <rafamartnogueira@gmail.com>
2026-06-09 22:30:24 -0300

    Atividade não finalizada.

---
+++ tests/atividade.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+const MEU_RM   = 'COLOQUE AQUI SEU RM'
+const BASE_URL = 'https://prova.carvalho.cc';
+
+test('submissão completa da prova', async ({ browser }) => {
+  const context = await browser.newContext({
+    extraHTTPHeaders: { 'x-exam-student-id': MEU_RM },
+  });
+  const page = await context.newPage();
+
+  await page.goto(BASE_URL);
+
+  // Dry-run banner should NOT appear
+  await expect(page.locator('#dry-run-banner')).not.toBeVisible();
+
+  // escreva o teste a partir daqui
+
+await page.getByLabel("Nome Completo").click();
+
+await page.fill("")
+
+
+  
+
+  // Confirmar envio
+  // Remover o comentário quando terminar de escrever o código
+  // await expect(page.getByRole('heading', { name: 'Prova enviada!' })).toBeVisible();
+});
